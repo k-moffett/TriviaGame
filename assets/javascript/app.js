da7cfaeb399c436d143b843a567437d7e8344c1e
Kurtis Moffett <moffkr91@gmail.com>
2018-03-01 10:13:23 -0800

    quicksave

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,45 @@
 $( document ).ready(function () {
 
-console.log("yo")
-return "yo"
+let game = {
+    time: "",
+    start:
+        function start(){
+            game.time = 30
+            $("#time").text(game.time)
+            display_time()
+        },
+    countdown:
+        function countdown() {
+
+        },
+}
 
 
+
+
+
+function display_time() {
+    while (game.time > 0) {
+        setInterval( game.countdown, 1000)
+        $("#time").text(game.time)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+game.start()
+//setTimeout(function(){
+//alert("You Lose!")
+//}, 1000)
+
+//////////
 })
